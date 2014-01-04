1d7c0b36596e59139f2e8206831ab1bb1f026b2f
nghia11110 <nghianguyen248@gmail.com>
2014-01-05 06:14:52 +0700

    Nghia
    Demo

--- javascript/demo.js
+++ javascript/demo.js
@@ -48,4 +48,45 @@ $(document).ready(function() {
     if (width > 480 && menu.is(':hidden'))
       menu.removeAttr('style');
   });
+  window.onload=function(){
+		init();
+	};
 });
+function init(){
+	$("#content-wrapper #whatwedo .technology .app").mouseover(function(){
+		$("#content-wrapper #whatwedo img:nth-child(1)").show();
+		$("#content-wrapper #whatwedo img:nth-child(2)").hide();
+		$("#content-wrapper #whatwedo img:nth-child(3)").hide();
+		$("#content-wrapper #whatwedo .technology .web img").show();
+		$("#content-wrapper #whatwedo .technology .design img").show();
+	});
+	$("#content-wrapper #whatwedo .technology .web").mouseover(function(){
+		$("#content-wrapper #whatwedo img:nth-child(2)").show();
+		$("#content-wrapper #whatwedo img:nth-child(1)").hide();
+		$("#content-wrapper #whatwedo img:nth-child(3)").hide();
+		$("#content-wrapper #whatwedo .technology .app img").show();
+		$("#content-wrapper #whatwedo .technology .design img").show();
+	});
+	$("#content-wrapper #whatwedo .technology .design").mouseover(function(){
+		$("#content-wrapper #whatwedo img:nth-child(3)").show();
+		$("#content-wrapper #whatwedo img:nth-child(1)").hide();
+		$("#content-wrapper #whatwedo img:nth-child(2)").hide();
+		$("#content-wrapper #whatwedo .technology .app img").show();
+		$("#content-wrapper #whatwedo .technology .web img").show();
+	});
+	
+	//init canvas
+	_resultCanvas = document.getElementById("whoweare-canvas");
+	_resultContext = _resultCanvas.getContext("2d");
+	_resultContext.font = " 15pt Calibri";
+	 _resultContext.fillStyle="white";
+	_resultContext.fillText("Xin chao tat ca moi nguoi chung toi la volcano!",150,40);
+	
+	$("#content-wrapper #whoweare .member .nghia").mouseover(function(){
+		$("#content-wrapper #whoweare .member .nghia img:nth-child(1)").show();
+		$("#content-wrapper #whoweare .member .nghia img:nth-child(2)").fadeIn();
+	});
+	$("#content-wrapper #whoweare .member .nghia").mouseout(function(){
+		$("#content-wrapper #whoweare .member .nghia img:nth-child(1)").hide();
+	});
+}
